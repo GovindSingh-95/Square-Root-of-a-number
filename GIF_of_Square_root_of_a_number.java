95a84e4cd1118b2d98d080b6193cd9cd4ff466ab
Govind Singh <156602160+GovindSingh-95@users.noreply.github.com>
2024-08-13 13:11:40 +0530

    Code.java

--- GIF_of_Square_root_of_a_number.java
+++ GIF_of_Square_root_of_a_number.java
@@ -1,2 +1,24 @@
-public class GIF_of_Square_root_of_a_number {
+import java.util.*;
+public class GIF_of_Square_root_of_a_number{
+  public static void main(String[] args){
+    Scanner Sc=new Scanner(System.in);
+    System.out.print("Enter number for Square root: ");
+    int n=Sc.nextInt();
+    System.out.println("The GIF of square root of "+n+" is "+answer(n));
+  }
+  static int answer(int n){
+    if(n==0||n==1)
+      return n;
+    int low=1,high=n;
+    while(low<=high){
+      int mid=low+(high-low)/2;
+      if(mid==n/mid)
+        return mid;
+      else if(mid>n/mid)
+        high=mid-1;
+      else
+        low=mid+1;
+    }
+    return high;
+  }
 }
